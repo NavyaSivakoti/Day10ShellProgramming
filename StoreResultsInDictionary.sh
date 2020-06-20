eb0e505773e2c4423c22b43be06ec78d6c45c9af
NavyaSivakoti <lakshminavya104@gmail.com>
2020-06-20 20:27:03 +0530

    uc-6 branch created and StoreResultsInDictionary.sh

---
+++ StoreResultsInDictionary.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+echo "Enter 3 numbers"
+read -p "Enter a number: " a
+read -p "Enter number b: " b
+read -p "Enter number c: " c
+
+
+echo $a $b $c
+
+uc2=$((a+b*c))
+echo $uc2
+
+uc3=$((a*b+c))
+echo $uc3
+
+uc4=$((c+a/b))
+echo $uc4
+
+uc5=$((a%b+c))
+echo $uc5
+
+declare -A Computations
+Computation[uc2]=$uc2
+Computation[uc3]=$uc3
+Computation[uc4]=$uc4
+Computation[uc5]=$uc5
+
+
+
+
+
+echo "All values" ${Computation[@]}
